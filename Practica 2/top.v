b3657b931f80d3a4addbff159f8cbbfdfebf5d00
Lego <82170264+dizzy11ghost@users.noreply.github.com>
2026-03-03 08:41:33 -0600

    added brief project description

--- Practica 2/top.v
+++ Practica 2/top.v
@@ -1,3 +1,7 @@
+//Sophia Leñero Gómez A01639462
+//Diseño con lógica programable (Gpo 101)
+//Practica 2 - BCD 4 displays. El siguiente código sirve para usar switches de una FPGA y usar el 
+//display de 7 segmentos para mostrar el número ingresado, separado en unidades, decenas, centenas y millares 
 module top(
     input [9:0] SW,
     output [0:6] HEX0, HEX1, HEX2, HEX3
