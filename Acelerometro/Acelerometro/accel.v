0ad2c7bc43031ecf6b78640d86a3c362aa2af56c
soph-lego <a01639462@tec.mx>
2026-03-06 19:19:04 -0600

    Removed accel as a top entity module, changed it to be instantiable

--- Acelerometro/Acelerometro/accel.v
+++ Acelerometro/Acelerometro/accel.v
@@ -3,22 +3,19 @@
 //===========================================================================
 
 module accel (
-   input          ADC_CLK_10,
-   input          MAX10_CLK1_50,
-   input          MAX10_CLK2_50,
-   output [7:0]   HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
-   input  [1:0]   KEY,
-   output [9:0]   LEDR,
-   input  [9:0]   SW,
-   output         GSENSOR_CS_N,
-   input  [2:1]   GSENSOR_INT,
-   output         GSENSOR_SCLK,
-   inout          GSENSOR_SDI,
-   inout          GSENSOR_SDO,
-   output [9:0]   ARDUINO_IO
+	input clk_10,
+	input clk,
+	input clk_2,
+	input	rst,
+	output sensor,
+	input [2:1] sensor_int,
+	output sensor_sclk,
+	inout sensor_sdi,
+	inout sensor_sdo,
+	output pwm_out_x, pwm_out_y_1, pwm_out_y_2, pwm_out_z
 );
 
-//===== Parámetros
+// Parámetros
    localparam SPI_CLK_FREQ = 200;
    localparam UPDATE_FREQ  = 1;
    localparam IN_MIN  = 7;
@@ -28,55 +25,58 @@ module accel (
 	localparam Z_IN_MIN  = 5;
 	localparam Z_IN_MAX  = 170;
 
-//===== Señales
-   wire reset_n;
-   wire clk, spi_clk, spi_clk_out;
+// Señales
+   wire clk_0, spi_clk, spi_clk_out;
    wire data_update;
    wire [15:0] data_x, data_y, data_z;
 
-//===== PLL
+// PLL
 PLL ip_inst (
-   .inclk0     (MAX10_CLK1_50),
-   .c0         (clk),
+   .inclk0     (clk),
+   .c0         (clk_0),
    .c1         (spi_clk),
    .c2         (spi_clk_out)
 );
 
-//===== SPI / Acelerómetro
+// SPI / Acelerómetro
 spi_control #(
       .SPI_CLK_FREQ (SPI_CLK_FREQ),
       .UPDATE_FREQ  (UPDATE_FREQ))
    spi_ctrl (
-      .reset_n     (reset_n),
-      .clk         (clk),
+      .reset_n     (rst),
+      .clk         (clk_0),
       .spi_clk     (spi_clk),
       .spi_clk_out (spi_clk_out),
       .data_update (data_update),
       .data_x      (data_x),
       .data_y      (data_y),
       .data_z      (data_z),
-      .SPI_SDI     (GSENSOR_SDI),
-      .SPI_SDO     (GSENSOR_SDO),
-      .SPI_CSN     (GSENSOR_CS_N),
-      .SPI_CLK     (GSENSOR_SCLK),
-      .interrupt   (GSENSOR_INT)
+      .SPI_SDI     (sensor_sdi),
+      .SPI_SDO     (sensor_sdo),
+      .SPI_CSN     (sensor),
+      .SPI_CLK     (sensor_sclk),
+      .interrupt   (sensor_int)
 );
 
-//===== Reset y relojes
-assign reset_n = KEY[0];
-
-wire clk_2_hz, clk_50_hz;
+// Relojes
+wire clk_2_hz, clk_60_Hz, clk_50_hz;
 
 clk_divider #(.FREQ(2)) DIVISOR_REFRESH (
-   .clk    (MAX10_CLK1_50),
-   .rst    (~reset_n),
+   .clk    (clk),
+   .rst    (~rst),
    .clk_div(clk_2_hz)
 );
 
 clk_divider #(.FREQ(50)) DIVISOR (
-   .clk    (MAX10_CLK1_50),
-   .rst    (~reset_n),
+   .clk    (clk),
+   .rst    (~rst),
    .clk_div(clk_50_hz)
+);
+
+clk_divider #(.FREQ(60)) DIVISOR_60 (
+   .clk    (clk),
+   .rst    (~rst),
+   .clk_div(clk_60_hz)
 );
 
 //Muestreo de datos
@@ -107,28 +107,30 @@ always @(posedge clk_50_hz) begin
 		(smooth_x > angle_x) smooth_x <= smooth_x - ((smooth_x - angle_x) >> 3);
 end
 
-always @(posedge clk_50_hz) begin
+always @(posedge clk_60_hz) begin
    if      
-		(smooth_y_1 < angle_y_1) smooth_y_1 <= smooth_y_1 + ((angle_y_1 - smooth_y_1) >> 3);
+		(smooth_y_1 < angle_y_1) smooth_y_1 <= smooth_y_1 + ((angle_y_1 - smooth_y_1) >> 5);
    else if 
-		(smooth_y_1 > angle_y_1) smooth_y_1 <= smooth_y_1 - ((smooth_y_1 - angle_y_1) >> 3);
+		(smooth_y_1 > angle_y_1) smooth_y_1 <= smooth_y_1 - ((smooth_y_1 - angle_y_1) >> 5);
+end
+
+always @(posedge clk_60_hz) begin
+   if      
+		(smooth_y_2 < angle_y_2) smooth_y_2 <= smooth_y_2 + ((angle_y_2 - smooth_y_2) >> 5);
+   else if 
+		(smooth_y_2 > angle_y_2) smooth_y_2 <= smooth_y_2 - ((smooth_y_2 - angle_y_2) >> 5);
 end
 
 always @(posedge clk_50_hz) begin
    if      
-		(smooth_y_2 < angle_y_2) smooth_y_2 <= smooth_y_2 + ((angle_y_2 - smooth_y_2) >> 3);
+		(smooth_z < angle_z) smooth_z <= smooth_z + ((angle_z - smooth_z) >> 4);
    else if 
-		(smooth_y_2 > angle_y_2) smooth_y_2 <= smooth_y_2 - ((smooth_y_2 - angle_y_2) >> 3);
+		(smooth_z > angle_z) smooth_z <= smooth_z - ((smooth_z - angle_z) >> 4);
 end
 
-always @(posedge clk_50_hz) begin
-   if      
-		(smooth_z < angle_z) smooth_z <= smooth_z + ((angle_z - smooth_z) >> 3);
-   else if 
-		(smooth_z > angle_z) smooth_z <= smooth_z - ((smooth_z - angle_z) >> 3);
-end
 
-//===== Mapeo
+
+// Mapeo
 wire [15:0] mapped_x, mapped_y_1, mapped_y_2, mapped_z;
 
 
@@ -137,33 +139,33 @@ assign mapped_y_1 = ((smooth_y_1 - IN_MIN) * (OUT_MAX - OUT_MIN)) / (IN_MAX - IN
 assign mapped_y_2 = OUT_MAX - mapped_y_1;
 assign mapped_z = ((Z_IN_MAX - smooth_z) * (OUT_MAX - OUT_MIN)) / (Z_IN_MAX - Z_IN_MIN);
 
-//===== PWM → servos
+// PWM → servos
 pwm #(.MIN(2), .MAX(13)) pwm_x (
-   .clk    (clk),
-   .rst_p  (~reset_n),
+   .clk (clk_0),
+   .rst_p (~rst),
    .pwm_in (mapped_x[7:0]),
-   .pwm_out(ARDUINO_IO[0])
+   .pwm_out(pwm_out_x)
 );
 
-pwm #(.MIN(2), .MAX(13)) pwm_y_1 (
-   .clk    (clk),
-   .rst_p  (~reset_n),
+pwm #(.MIN(2), .MAX(11)) pwm_y_1 (
+   .clk (clk_0),
+   .rst_p (~rst),
    .pwm_in (mapped_y_1[7:0]),
-   .pwm_out(ARDUINO_IO[1])
+   .pwm_out(pwm_out_y_1)
 );
 
-pwm #(.MIN(2), .MAX(13)) pwm_y_2 (
-   .clk    (clk),
-   .rst_p  (~reset_n),
+pwm #(.MIN(2), .MAX(11)) pwm_y_2 (
+   .clk (clk_0),
+   .rst_p (~rst),
    .pwm_in (mapped_y_2[7:0]),
-   .pwm_out(ARDUINO_IO[2])
+   .pwm_out(pwm_out_y_2)
 );
 
 pwm #(.MIN(2), .MAX(13)) pwm_z (
-   .clk    (clk),
-   .rst_p  (~reset_n),
+   .clk (clk__0),
+   .rst_p (~rst),
    .pwm_in (mapped_z[7:0]),
-   .pwm_out(ARDUINO_IO[3])
+   .pwm_out(pwm_out_z)
 );
 
 endmodule
