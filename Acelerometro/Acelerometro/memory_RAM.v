e6e7491ddb18a2ceda8a4dedfafd56181b867ab3
Lego Leñero <82170264+dizzy11ghost@users.noreply.github.com>
2026-03-09 00:00:11 -0600

    Refactor memory_RAM module for improved readability

--- Acelerometro/Acelerometro/memory_RAM.v
+++ Acelerometro/Acelerometro/memory_RAM.v
@@ -1,28 +1,27 @@
 module memory_RAM #(parameter NBits = 24, NAddr = 3)(
-
 	input clk, rst_a,
 	input wr_en,
 	input [NBits - 1 : 0] Data_in,
-	
 	input [NAddr - 1 : 0] Data_address,
-	
-	output reg [NBits - 1 : 0] Data_out
-
+	output [NBits - 1 : 0] Data_out
 );
 
-reg [NBits - 1 : 0] RAM [0 : (2**NAddr) - 1];
+	reg [NBits - 1 : 0] RAM [0 : (2**NAddr) - 1];
 
-always @(posedge clk or negedge rst_a)
-begin
-	if(!rst_a)
+	always @(posedge clk or negedge rst_a)
 	begin
-		RAM[Data_address] <= 'd0;
-	end
-	else if(wr_en)
+		if(!rst_a)
+		begin
+			integer i;
+			for(i = 0; i < 2**NAddr; i = i+1)
+				RAM[i] <= 'd0;
+		end
+		else if(wr_en)
 		begin
 			RAM[Data_address] <= Data_in;
 		end
-end
-assign Data_out = RAM[Data_address]; //lectura asíncrona, fuera del always
+	end 
+
+	assign Data_out = RAM[Data_address]; 
  
-endmodule 
+endmodule
